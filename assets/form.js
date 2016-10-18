069728680112b5af416e2bc763dd4a0d73e56f85
Sharol Chand <sharolchand@gmail.com>
2016-10-18 00:09:25 -0700

    cleaning comments

--- assets/form.js
+++ assets/form.js
@@ -20,38 +20,12 @@ $(document).on("ready", function(){
         };
 
         $.post('/main/form_data', postData, function(data){
-            //This should be JSON preferably. but can't get it to work on jsfiddle
-            //Depending on what your controller returns you can change the action
-            // if (data == 'PASS') {
-            //     alert('login pased');
-            // } else {
-            //     alert('login failed');
-            // }
+
             console.log(data);
 
-            $('.modal').modal('show') 
+            $('.modal').modal('show')
         });
 
-        // Returns successful data submission message when the entered information is stored in database.
-        // var dataString = 'first_name='+ first_name + '&last_name='+ last_name + '&start_date='+ start_date;
-
-        // if(first_name==''||last_name==''||start_date=='')
-        // {
-        //     alert("Please Fill All Fields");
-        // }
-        // else
-        // {
-        //     // AJAX Code To Submit Form.
-        //     $.ajax({
-        //         type: "POST",
-        //         url: "form_data",
-        //         data: dataString,
-        //         cache: false,
-        //         success: function(result){
-        //             alert(result);
-        //         }
-        //     });
-        // }
         return false;
     });
 
